b884924fff52cbd6278f824c6126e3308395c63e
arun <arunkumar413@gmail.com>
2024-02-08 23:44:11 +0530

    adding config file

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import appStore from './store/appStore.js';
-import { Provider } from 'react-redux';
-import { AppRouter } from './components/AppRouter.jsx';
-import { RouterProvider } from 'react-router-dom';
-import  '../src/App.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import appStore from "./store/appStore.js";
+import { Provider } from "react-redux";
+import { AppRouter } from "./components/AppRouter.jsx";
+import { RouterProvider } from "react-router-dom";
+import "./App.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={appStore}>
       <RouterProvider router={AppRouter} />
