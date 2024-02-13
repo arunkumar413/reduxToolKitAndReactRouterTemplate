a3b3994102b0df8bfc007ca46537ec7e3a6185f8
arun <arunkumar413@gmail.com>
2024-02-14 00:13:39 +0530

    saving for review

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,21 +1,28 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import { createBrowserRouter } from 'react-router-dom';
-import { Home } from './Home';
-import { About } from './About';
-import { Contact } from './contact';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter } from "react-router-dom";
+import { Home } from "./Home";
+import { About } from "./About";
+import { Contact } from "./contact";
 
-export const AppRouter = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/about',
-    element: <About />,
-  },
-  {
-    path: '/contact',
-    element: <Contact />,
-  },
-]);
+export const AppRouter = createBrowserRouter(
+  [
+    {
+      path: "/dist/index",
+      element: <Home />,
+    },
+    {
+      path: "/dist/about",
+      element: <About />,
+    },
+    {
+      path: "/dist/contact",
+      element: <Contact />,
+    },
+    {
+      path: "/dist/index",
+      element: <Home />,
+    },
+  ],
+  { basename: "/dist/index" }
+);
--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import copy from "rollup-plugin-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: path.resolve(__dirname, "./src"),
+    // outDir: path.resolve(__dirname, "./src"),
     rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split(".").at(1);
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = "img";
-          }
-          return `assets/${extType}/[name]-[hash][extname]`;
-        },
-        chunkFileNames: "assets/js/[name]-[hash].js",
-        entryFileNames: "assets/js/[name]-[hash].js",
-      },
-      input: {
-        app: "./src/index.html",
-      },
+      // output: {
+      //   assetFileNames: (assetInfo) => {
+      //     let extType = assetInfo.name.split(".").at(1);
+      //     if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+      //       extType = "img";
+      //     }
+      //     return `assets/${extType}/[name]-[hash][extname]`;
+      //   },
+      //   chunkFileNames: "assets/js/[name]-[hash].js",
+      //   entryFileNames: "assets/js/[name]-[hash].js",
+      // },
+      // input: {
+      //   app: "./src/index.html",
+      // },
     },
   },
-  plugins: [react()],
+  plugins: [react(), ,],
 });
